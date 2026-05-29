3423fd953c1772bdaf3af851df57932011d06f62
aastha9531 <aasthasingh9531@gmail.com>
2026-05-29 16:59:34 +0530

    Striver pattern questions

--- Patterns/Patterns.cpp
+++ Patterns/Patterns.cpp
@@ -393,6 +393,55 @@ void pattern18(int n)
 
 }
 
+    // Function to print Pattern 19
+    void pattern19(int N) {
+        // Initial spaces for upper half
+        int iniS = 0;
+
+        // Loop for upper half rows
+        for (int i = 0; i < N; i++) {
+            // Print stars on left
+            for (int j = 1; j <= N - i; j++) {
+                cout << "*";
+            }
+            // Print spaces in middle
+            for (int j = 0; j < iniS; j++) {
+                cout << " ";
+            }
+            // Print stars on right
+            for (int j = 1; j <= N - i; j++) {
+                cout << "*";
+            }
+            // Increase middle spaces by 2
+            iniS += 2;
+            // Move to next row
+            cout << endl;
+        }
+
+        // Initial spaces for lower half
+        iniS = 2 * N - 2;
+
+        // Loop for lower half rows
+        for (int i = 1; i <= N; i++) {
+            // Print stars on left
+            for (int j = 1; j <= i; j++) {
+                cout << "*";
+            }
+            // Print spaces in middle
+            for (int j = 0; j < iniS; j++) {
+                cout << " ";
+            }
+            // Print stars on right
+            for (int j = 1; j <= i; j++) {
+                cout << "*";
+            }
+            // Decrease middle spaces by 2
+            iniS -= 2;
+            // Move to next row
+            cout << endl;
+        }
+    }
+
 int main()
 {
     int n;
@@ -416,5 +465,6 @@ int main()
         pattern16(n);
         pattern17(n);
     pattern18(n);
+    pattern19(n);
     return 0;
 }
